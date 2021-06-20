b3f3a40070feccef89f309824a91fc92b0e5a952
Nil Andreu <nilandreug@gmail.com>
2021-06-20 22:40:36 +0200

    User app

---
+++ apps/users/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# To authenticate the user with auth app provided with Django
+from django.contrib.auth.forms import UserCreationForm # A form to sign in
+from django.contrib.auth import login, authenticate
+from django.views import View
+from django.shortcuts import render, redirect
+
+# Create your views here.
+# Class based view
+class SignupView(View):
+    template_name = "signup.html"
+
+    def post(self, request):
+        if form.is_valid():
+            form.save()
+            username = form.clened_data.get('username')
+            password = form.cleaned_data.get("password")
+            user = authenticate(
+                username = username,
+                password = password
+            ) # Here we authenticate the user with the cleaned data
+            login(request, user) #Login with the user that has authenticated
+            return redirect("/")
+
+    def get(self, request):
+        return render(request, self.template_name, {'form':UserCreationForm()})
