57bde4a7e899c9fce19a1da46c091ae4eca9c9a1
Nil Andreu <nilandreug@gmail.com>
2021-06-21 20:27:24 +0200

    Login and logout

--- Tizza/urls.py
+++ Tizza/urls.py
@@ -14,16 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
+from django.urls import path,include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pizzas/', include('apps.pizza.urls')),
     path('', include('apps.users.urls')), # For the login
-    path("login/", auth_views.login, name="login"), #The login view
+    path("login/", auth_views.LoginView.as_view(), name="login"), #The login view
     # By default django will attempt to render registration/login.html
-    
-    path("logout/", auth_views.logout, name="logout") 
+
+    path("logout/", auth_views.LogoutView.as_view(), name="logout") 
 ]
